9708205518ff1fb529fee50640d8436eb87dbe5e
Marcos A. López Cruz <marquibiris@gmail.com>
2020-03-25 01:08:20 -0400

    add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [require("tailwindcss"), require("postcss-preset-env")]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: ['./components/**/*.js', './pages/**/*.js'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        }
+      : {})
+  }
 };
