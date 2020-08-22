9700442a9db5bb82c8586dab84794c02df71b766
Marcos A. López Cruz <marquibiris@gmail.com>
2020-08-22 10:53:24 -0400

    data update

--- components/Footer.js
+++ components/Footer.js
@@ -45,12 +45,6 @@ export default function Footer() {
                 label='Directorio de SMICRC (Rastreo de contactos)'
               />
             </li>
-            <li className='ml-4 mb-2'>
-              <ExtLink
-                link='http://taskforceciudadano.com'
-                label='Task Force Ciudadano'
-              />
-            </li>
           </ul>
         </div>
         <div className='mb-6'>
